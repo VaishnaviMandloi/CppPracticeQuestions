c18bde1c385db496e847642da4b09015ca06223d
VaishnaviMandloi <98760492+VaishnaviMandloi@users.noreply.github.com>
2024-05-03 15:42:42 +0530

    Create 12_RemoveSpacesFromString.cpp

---
+++ Top-100-C++-Coding-Interview-Questions-and-Answers-(2024)/12_RemoveSpacesFromString.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+string removeSpace(string str){
+    
+    string res="";
+    
+    for(char c : str){
+        if(c!=' '){
+            res +=c; 
+        }
+    }
+    return res;
+}
+
+int main(){
+    
+    //string str="I am Vaishnavi Mandloi";
+    char str[100]={};
+    
+    cout<<"Enter string from which you want to remove spaces : ";
+    //cin>>str;
+    cin.getline(str,100);
+    
+    cout<<"String after remove spaces : "<<removeSpace(str)<<endl;
+    cout<<"String before removing space : "<<str;
+    
+    return 0;
+}
