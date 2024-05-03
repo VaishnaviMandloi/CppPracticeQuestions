84bba51cfabbb65b13ea5b0dc97bf528d55d0e36
VaishnaviMandloi <98760492+VaishnaviMandloi@users.noreply.github.com>
2024-05-03 20:58:46 +0530

    Create 17_CheckPalindrome.cpp

---
+++ Top-100-C++-Coding-Interview-Questions-and-Answers-(2024)/17_CheckPalindrome.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool palindrome(long num){
+    
+    string str= to_string(num);
+    
+    int len = str.size();
+    
+    for(int i=0;i<len/2;i++){
+        if(str[i]!=str[len-1-i]){
+            return false;
+        }
+        num/=10;
+    }
+    
+    return true;
+}
+
+int main(){
+    
+    long num;
+    
+    cout<<"Enter the number to check it is palindrome or not : ";
+    cin>>num;
+    
+    cout<<num<<(palindrome(num)?" is a palindrome number":" is not a palindrome number");
+    
+    
+    return 0;
+}
